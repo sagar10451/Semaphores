1362a17de347c3ca405208ed534411620da6f50c
Sagar Kumar <sagarkumar@192.168.1.4>
2024-08-28 17:37:18 +0530

    Initial commit

---
+++ src/main/java/org/example/SharedResource.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.concurrent.Semaphore;
+
+public class SharedResource {
+    boolean isAvailable=false;
+    Semaphore lock = new Semaphore(2);
+
+    public void producer(){
+        try{
+            lock.acquire();
+            System.out.println("Lock acquired by: "+ Thread.currentThread().getName());
+            isAvailable=true;
+            Thread.sleep(3000);
+
+        }catch(Exception e){
+            //some code
+        }
+        finally {
+            System.out.println("Lock released by: "+ Thread.currentThread().getName());
+            lock.release();
+        }
+    }
+}
